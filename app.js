287a31ff016c536a7535e80bdece4319e6e37179
UniBreakfast <unibreakfast1@gmail.com>
2023-01-04 14:19:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+import { authors } from './authors.js';
+import { books } from './books.js';
---
+++ authors.js
@@ -0,0 +1,58 @@
+export const authors = [
+  {
+    id: 1,
+    firstname: 'Joan',
+    lastname: 'Rowling',
+    middlename: 'Kathleen',
+    dob: '1965-07-31',
+    books: []
+  },
+  {
+    id: 2,
+    firstname: 'John',
+    lastname: 'Tolkien',
+    middlename: 'Ronald Reuel',
+    dob: '1892-01-03',
+    books: []
+  },
+  {
+    id: 4,
+    firstname: 'George',
+    lastname: 'Martin',
+    middlename: 'Raymond Richard',
+    dob: '1948-09-20',
+    books: []
+  },
+  {
+    id: 5,
+    firstname: 'Brandon',
+    lastname: 'Sanderson',
+    middlename: 'Dale',
+    dob: '1975-12-19',
+    books: []
+  },
+  {
+    id: 6,
+    firstname: 'Patrick',
+    lastname: 'Rothfuss',
+    middlename: 'Rafael',
+    dob: '1973-06-06',
+    books: []
+  },
+  {
+    id: 7,
+    firstname: 'Neil',
+    lastname: 'Gaiman',
+    middlename: 'Richard',
+    dob: '1960-11-10',
+    books: [],
+  },
+  {
+    id: 8,
+    firstname: 'Robert',
+    lastname: 'Jordan',
+    middlename: 'James',
+    dob: '1948-10-17',
+    books: [],
+  }
+];
