877103b5399645a82ae5ab2623a3eff4cfd103db
UniBreakfast <unibreakfast1@gmail.com>
2023-01-06 11:55:38 +0200

    introduce genres

--- js/render-author-books.js
+++ js/render-author-books.js
@@ -2,19 +2,24 @@ export { renderAuthorBooks };
 
 import { authors } from '../data/authors.js';
 import { books } from '../data/books.js';
+import { genres } from '../data/genres.js';
 
 function renderAuthorBooks(id) {
   const author = authors.find(author => author.id == id);
-  return books.filter(book => author.books.includes(book.id)).map(book => `
-    <tr data-id="${book.id}">
-      <td>${book.title}</td>
-      <td>${book.pages}</td>
-      <td>${book.genre}</td>
-      <td>
-        <button class="edit-book-btn">Edit</button>
-        <button class="delete-book-btn">Delete</button>
-        <button class="view-book-btn">View</button>
-      </td>
-    </tr>
-  `).join('');
+  return books.filter(book => author.books.includes(book.id)).map(book => {
+    const genreNames = genres.filter(genre => book.genres.includes(genre.id)).map(genre => genre.name);
+
+    return `
+      <tr data-id="${book.id}">
+        <td>${book.title}</td>
+        <td>${book.pages}</td>
+        <td>${genreNames.join(', ')}</td>
+        <td>
+          <button class="edit-book-btn">Edit</button>
+          <button class="delete-book-btn">Delete</button>
+          <button class="view-book-btn">View</button>
+        </td>
+      </tr>
+    `
+  }).join('');
 }
--- js/render-books.js
+++ js/render-books.js
@@ -2,6 +2,7 @@ export { renderBooks };
 
 import { authors } from '../data/authors.js';
 import { books } from '../data/books.js';
+import { genres } from '../data/genres.js';
 import { booksTBody } from './elements.js';
 import { formatName } from './render-authors.js';
 
@@ -11,13 +12,14 @@ function renderBooks() {
 
 function buildRow(book) {
   const authorNames = authors.filter(author => author.books.includes(book.id)).map(formatName);
+  const genreNames = genres.filter(genre => book.genres.includes(genre.id)).map(genre => genre.name);
 
   return `
     <tr data-id="${book.id}">
       <td>${book.title}</td>
       <td>${authorNames.join(', ')}</td>
       <td>${book.pages}</td>
-      <td>${book.genre}</td>
+      <td>${genreNames.join(', ')}</td>
       <td>
         <button class="edit-btn">Edit</button>
         <button class="del-btn">Remove</button>
